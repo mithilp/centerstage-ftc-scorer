70d0fca5f73c8f227b63d40f660b6d34ddd45164
mithilp <ping.mithil@gmail.com>
2023-12-31 13:09:52 -0800

    Header w/ Stopwatch & Timer

---
+++ src/components/Timer.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+const Timer = () => {
+	const [time, setTime] = useState(150);
+	const [ticking, setTicking] = useState(false);
+
+	const toggleTicking = () => {
+		setTicking(!ticking);
+	};
+
+	const resetTimer = () => {
+		setTime(150);
+	};
+
+	useEffect(() => {
+		let myInterval = setInterval(() => {
+			if (ticking && time > 0) {
+				setTime(time - 1);
+			} else if (ticking && time === 0) {
+				setTicking(false);
+			} else {
+				clearInterval(myInterval);
+			}
+		}, 1000);
+		return () => {
+			clearInterval(myInterval);
+		};
+	});
+
+	const formatTime = (secs: number) => {
+		let divisor_for_minutes = secs % (60 * 60);
+		let minutes = Math.floor(divisor_for_minutes / 60);
+
+		let divisor_for_seconds = divisor_for_minutes % 60;
+		let seconds = Math.ceil(divisor_for_seconds);
+
+		return `${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+	};
+
+	return (
+		<div className="space-y-1">
+			<h2 className="text-xs tracking-widest uppercase text-gray-400 leading-none font-black text-center">
+				Timer
+			</h2>
+			<h3 className="text-4xl leading-none font-black text-center">
+				{formatTime(time)}
+			</h3>
+			<div className="flex space-x-2">
+				<button
+					className="transition bg-gray-600 hover:bg-gray-700 active:bg-gray-800 px-2 rounded-lg"
+					onClick={toggleTicking}
+				>
+					{ticking ? "Stop" : "Start"}
+				</button>
+				<button
+					className="transition bg-gray-600 hover:bg-gray-700 active:bg-gray-800 px-2 rounded-lg"
+					onClick={resetTimer}
+				>
+					Reset
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default Timer;
